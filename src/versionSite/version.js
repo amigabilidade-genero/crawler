2d3e249d4755d1a1d90945e490bae36fa9a9eebe
Paulo Henrique Santos <paulohenriquercs@gmail.com>
2018-07-25 23:11:48 -0300

    Testando versions

---
+++ src/versionSite/version.js
@@ -0,0 +1,5 @@
+const Version = (version) => {
+  console.log(version)
+}
+
+export default Version
--- src/webDriver/masterWebDriver.js
+++ src/webDriver/masterWebDriver.js
@@ -1,19 +1,41 @@
 import 'chromedriver'
-import WebDriver from 'selenium-webdriver'
+import chrome from 'selenium-webdriver/chrome'
+import { Builder, By } from 'selenium-webdriver'
+import versionSite from '../versionSite/version'
 
-const URL = 'https://www.jusbrasil.com.br/jurisprudencia/busca?q=assédio+2017'
-const driver = new WebDriver.Builder().forBrowser('chrome').build()
+const URL = 'https://www.jusbrasil.com.br/jurisprudencia/busca?q=assédio+Belo+Horizonte+2017'
+
+const chromeOptions = new chrome.Options()
+chromeOptions.addArguments('test-type')
+chromeOptions.addArguments('start-maximized')
+chromeOptions.addArguments('--js-flags=--expose-gc')
+chromeOptions.addArguments('--enable-precise-memory-info')
+chromeOptions.addArguments('--disable-popup-blocking')
+chromeOptions.addArguments('--disable-default-apps')
+chromeOptions.addArguments('--disable-infobars')
+chromeOptions.addArguments('--incognito')
+chromeOptions.addArguments('--disable-extensionss')
+chromeOptions.addArguments('--no-sandbox')
+chromeOptions.addArguments('test-type=browser')
+
+const driver = new Builder()
+  .forBrowser('chrome')
+  .withCapabilities(chromeOptions)
+  .build()
+var VERSION = ''
 
 const MasterWebDriver = () => {
   initializaeBrowser()
   fetchUrl()
+  versionSite(VERSION)
 }
 
 const initializaeBrowser = async () => {
   try {
     await driver.manage().deleteAllCookies()
   } catch (error) {
-    console.log('Server error: Ocorreu um erro inesperado na inicialização do arquivo binário respectivo ao navegador.')
+    console.log('Server error: Ocorreu um erro inesperado na inicialização do arquivo binário respectivo ao navegador.'
+    )
     console.log('Erro interno: ', error)
     await driver.close()
   }
@@ -23,12 +45,35 @@ const fetchUrl = async () => {
   try {
     await driver.get(URL)
     console.log('Success: ', URL)
-  } catch (error) {
-    console.log('ERROR: ', error)
-    await driver.close()
+    initV1()
+  } catch (ERROR) {
+    console.log('ERROR: ', ERROR)
   } finally {
     await driver.close()
-    console.log('Close browser.')
+  }
+}
+
+const initV1 = async () => {
+  try {
+    var searchForm = await driver.findElement(
+      By.className('SearchResults-documents')
+    )
+    console.log(searchForm)
+  } catch (ERROR) {
+    console.log('Old version faill')
+    console.log('Call new version')
+    initV2()
+  }
+}
+
+const initV2 = async () => {
+  try {
+    console.log('New version change')
+    var searchForm = await driver.findElement(By.className('DocumentSnippet'))
+    console.log(searchForm)
+  } catch (ERROR) {
+    console.log('New version faill')
+    console.log('ERROR: ', ERROR)
   }
 }
 
